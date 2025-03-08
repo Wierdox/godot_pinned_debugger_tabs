f7f1c6f8855ecde2ace5f69cc38160e8fdf7cd64
Wierdox <104049283+Wierdox@users.noreply.github.com>
2025-03-07 17:17:31 -0800

    Fix critical error caused by changes in Godot 4.4. Also some minor cleaning.

--- addons/pinned_debugger_tabs/README.md
+++ addons/pinned_debugger_tabs/README.md
@@ -6,8 +6,9 @@ To pin a tab, press the newly added Pin Tab button on the Debugger tab bar.
 You can unpin by either pressing Unpin, or pressing the blue pin on the pinned tab.
 You can also shift the pin by going to a new tab and pressing Shift Pin.
 
+Changelog:
+*1.1: For 4.4+, fixed plugin critically failing due to changes in Debugger node hierarchy. Still works with all previous versions.
+
 This plugin overrides `run/output/always_open_output_on_play`.
 
 If the plugin seems to fail, try editing the first variable `time_until_open`, it might depend on your PC.
-
-For 3.x users, make note that this plugin will cancel out the yellow/red icon on Errors tab if pinned, and the pin may disapear on project run. This is only visual though. And due to the feature not existing, pressing the blue pin on a pinned tab will do nothing.
--- addons/pinned_debugger_tabs/pinned_debugger_tabs.gd
+++ addons/pinned_debugger_tabs/pinned_debugger_tabs.gd
@@ -1,24 +1,27 @@
 @tool
 extends EditorPlugin
 
-var time_until_open  := 0.1 #Change if needed
+var time_until_open  := 0.1 # Change if it doesn't seem to work.
 var pin_button :Control
 var pin_button_index :int
 var pin_index        :int
 var pin_texture_on  :Texture2D
 var pin_texture_off :Texture2D
 
-# Editor nodes, don't free
+# Editor nodes, don't free.
 var debugger :Node
 var debugger_tabs :TabContainer
 
 
 func _enter_tree() -> void:
-	# Get bottom panel as ref
+	# Get bottom panel as ref.
 	var bottom_control := Control.new()
 	var dumb_ref :Control= add_control_to_bottom_panel(bottom_control, "")
-	# Find relative paths (use Editor Debugger plugin if you ever need to do this)
-	debugger = dumb_ref.get_parent().get_parent().get_parent().get_child(1)
+	# Find relative paths (use Editor Debugger plugin if you ever need to do this).
+	if Engine.get_version_info().hex >= 0x040400: # 4.4 and later.
+		debugger = dumb_ref.get_parent().get_parent().get_parent().get_parent().get_child(1)
+	else:
+		debugger = dumb_ref.get_parent().get_parent().get_parent().get_child(1)
 	debugger_tabs = debugger.get_child(0).get_child(0).get_child(0)
 	remove_control_from_bottom_panel(bottom_control)
 	
@@ -53,7 +56,7 @@ func _on_debugger_tab_button_pressed(tab:int) -> void:
 			enable_unpin()
 		return
 	if debugger_tabs.get_tab_button_icon(tab) == null:
-		return # For some reason tabs without a texture can still emit tab_button_pressed
+		return # For some reason tabs without a texture can still emit tab_button_pressed (fixed in later versions).
 	debugger_tabs.set_tab_button_icon(tab, null)
 	pin_index = -1
 	enable_pin()
@@ -84,7 +87,7 @@ func enable_unpin() -> void:
 	debugger_tabs.set_tab_title(pin_button_index, "Unpin")
 	debugger_tabs.set_tab_icon(pin_button_index, pin_texture_off)
 
-# Virtual called before launching game
+# Virtual called before launching game.
 func _build() -> bool:
 	open_pinned_tab()
 	return true
@@ -99,9 +102,10 @@ func open_pinned_tab() -> void:
 
 
 func _exit_tree() -> void:
-	# Clean up
 	if pin_index != -1:
 		debugger_tabs.set_tab_button_icon(pin_index, null)
+	
 	debugger_tabs.tab_button_pressed.disconnect(_on_debugger_tab_button_pressed)
 	debugger_tabs.tab_changed.disconnect(_on_debugger_tab_changed)
+	
 	pin_button.queue_free()
